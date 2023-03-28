7be3195b1e9df6aa9868bc2fc4d2dfd7662ce951
Wagner Alencar <wagalencar1998@outlook.com>
2023-03-28 11:11:32 -0300

    meu primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'om4z95',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
